78998669fc0e52f7778c79131e8223681a6e55a0
135791maria <dossantosmariadapenha05@gmail.com>
2024-01-06 13:57:56 -0300

    repositorio react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import React, { useState, useRef } from "react"
+import template7 from "./Assets/template7.png";
+import seta from "./Assets/seta.png";
+import lixeira from "./Assets/lixeira.png";
+import {
+  Container,
+  Image,
+  H1,
+  ContainerItens,
+  InputLabel,
+  Input,
+  Button,
+  User,
+
+} from "./styles"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [users, setUsers] = useState([]);
+  const inputName = useRef()
+  const inputAge = useRef()
 
-export default App;
+
+  async function addNewUser() {
+    const data = await axios.post("http://localhost:3001/users", {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+     
+    });
+     console.log(data)
+
+    //setUsers([
+    //  ...users, 
+    // { 
+    // id: Math.random(),
+    //   name: inputName.current.value, 
+    //   age: inputAge.current.value 
+    //  },
+    // ])
+
+  }
+  function deleteUser(userid) {
+    const newUsers = users.filter(user => user.id !== userid)
+    setUsers(newUsers);
+
+  }
+
+
+  return (
+
+    <Container>
+      <Image alt="logo-image" src={template7} />
+      <ContainerItens>
+        <H1>Olá!</H1>
+
+        <InputLabel >Nome</InputLabel>
+        <Input ref={(inputName)} Placeholder="Nome" />
+
+        <InputLabel>Idade</InputLabel>
+        <Input ref={(inputAge)} Placeholder="idade" />
+
+        <Button onClick={addNewUser}>Cadastrar <img alt="arrow" src={seta} />
+        </Button>
+
+        <ul>
+          {users.map(user => (
+            <User Key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+              <button onClick={() => deleteUser(user.id)}>
+                <img src={lixeira} alt="lata de lixo" />
+              </button>
+            </User>
+          ))
+          }
+        </ul>
+
+      </ContainerItens>
+    </Container>
+  );
+
+}
+export default App
+
